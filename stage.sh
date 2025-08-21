1378d3f9a325e65d269e5c3ef80a933307cf7a6c
Mike Park <mike@flexcompute.com>
2025-08-21 16:49:29 +0000

    stage builds libcaps.so and libocsm.so with RPATH for Python plugins

--- stage.sh
+++ stage.sh
@@ -18,8 +18,15 @@ export CASROOT=${PWD}/ESP/OpenCASCADE-${CASREV}.1
 
 export RPATH=-Wl,-rpath='$$ORIGIN:$$ORIGIN/../lib',--disable-new-dtags
 
-( cd ${esp}/EngSketchPad/src/EGADS/src && make clean && \
-  make RPATH=\'${RPATH}\' )
+( cd ${esp}/EngSketchPad/src/EGADS/src && \
+  make clean && \
+  make ${ESP_ROOT}/lib/libegads.so RPATH=\'${RPATH}\' )
+( cd ${esp}/EngSketchPad/src/CAPS/src && \
+  make clean && \
+  make ${ESP_ROOT}/lib/libcaps.so RPATH=\'${RPATH}\' )
+( cd ${esp}/EngSketchPad/src/OpenCSM && \
+  make clean && \
+  make ${ESP_ROOT}/lib/libocsm.so RPATH=\'${RPATH}\' )
 
 cp ${esp}/EngSketchPad/lib/* lib
 cp -r ${esp}/EngSketchPad/include include
